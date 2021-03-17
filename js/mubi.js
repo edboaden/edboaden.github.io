ab204a3ceb9cfb2207f29390e294f8b1246d4623
Ed Boaden <edboaden@gmail.com>
2021-03-17 08:44:52 +0000

    Display reviews.

--- js/mubi.js
+++ js/mubi.js
@@ -26,16 +26,16 @@ function showFilms(obj) {
 	const ratings = obj;
 
 	for (let i = 0; i < ratings.length; i++) {
+
+		// list item
 		const film = document.createElement('li');
 
+		// film
 		const filmTitle = document.createElement('a');
 		const filmYear = document.createElement('span');
 		const filmDirector = document.createElement('ul');
 		const filmDirectorIntro = document.createElement('span');
 		const filmImage = document.createElement('img');
-
-		const review = 	document.createElement('span');
-		const rating = document.createElement('span');
 
 		const directors = ratings[i].film.directors;
 		for (let j = 0; j < directors.length; j++) {
@@ -52,19 +52,24 @@ function showFilms(obj) {
 		filmTitle.href = ratings[i].film.canonical_url;
 		filmImage.src = ratings[i].film.stills.medium;
 
+		// rating
+		const review = 	document.createElement('blockquote');
+		const rating = document.createElement('span');
+
 		review.textContent = ratings[i].body;
 		rating.textContent = ratings[i].overall + ' / 5';
 
 		filmDirectorIntro.textContent = 'Directed by '
 		filmDirector.classList.add('director-list')
 
-		// film.appendChild(filmImage);
+		film.appendChild(filmImage);
 		film.appendChild(filmTitle);
 		film.appendChild(filmDirectorIntro);
 		film.appendChild(filmDirector);
 		film.appendChild(filmYear);
-		// film.appendChild(review);
+
 		film.appendChild(rating);
+		film.appendChild(review);
 
 		filmList.appendChild(film);
 	}
